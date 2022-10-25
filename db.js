307d6f5235ab7328c7d7e00c9dbb3eebc1eef84a
Oleksandr <88oleksandr@gmail.com>
2022-10-25 19:56:59 +0300

    heroku CLI

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ import { Sequelize } from 'sequelize';
 const sequelize = new Sequelize(
   'd786o8etkud9jk', // NAME DB
   'kbelfgxbdipyuv', // User
-  '33b622f92ca3b984c7f295abb1037ddebaca5645751791fc3d6cbe2f1cc5645c', // PASSWORD
+  '33b622f92ca3b984c7f295abb1037ddebaca5645751791fc3d6cbe2f1cc5645c',
   {
     dialect: 'postgres',
     host: 'ec2-54-76-43-89.eu-west-1.compute.amazonaws.com',
